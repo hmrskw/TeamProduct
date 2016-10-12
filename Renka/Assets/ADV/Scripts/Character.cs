d9ce8c00ec94cbc8b1a1ab0d3d0a8a4997342fde
busters0902 <busters0902@hotmail.co.jp>
2016-10-12 16:03:24 +0900

    背景、キャラクターの描画と切り替え

---
+++ Renka/Assets/ADV/Scripts/Character.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject face;
+
+    [SerializeField]
+    public GameObject clothes;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
