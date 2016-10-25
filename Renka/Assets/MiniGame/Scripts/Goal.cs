5eb82803ffeac5ac466b3c966d67dac0ed61bcee
PS15A05\PS15A kamimura <kamimurayuki0717@gmail.com>
2016-10-25 20:11:09 +0900

    ミニゲームの作成

---
+++ Renka/Assets/MiniGame/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+    [SerializeField]
+    GameObject goalPopUp;
+
+    public bool isGoal=false;
+        
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.tag == "Player")
+        {
+            goalPopUp.SetActive(true);
+            isGoal = true;
+            
+
+        }
+
+
+    }
+}
---
+++ Renka/Assets/MiniGame/Scripts/Player.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private StageManager stage = null;
+
+    [HideInInspector]
+    public int hp;
+    [SerializeField]
+    private GameObject[] hpImage;
+
+    [SerializeField]
+    private GameObject gameOverText;
+
+    private bool is_hit = false;
+    // Use this for initialization
+    void Start()
+    {
+        hp = hpImage.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //hpが0になったら
+      if(hp<=0)
+        {
+            //ステージスクロールをとめる
+            stage.ScrollSpeed = 0.0f;
+            gameOverText.SetActive(true);
+        }
+
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.tag == "Obstacle")
+        {
+            other.GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine(Damage());
+            
+        }
+
+        
+    }
+
+    IEnumerator Damage()
+    {
+        //ステージの移動を止める
+        stage.ScrollSpeed = 0.0f;
+        if(hp>0)
+        {
+            //HPの表示を1つ減らす
+            hpImage[hp - 1].SetActive(false);
+
+        }
+        hp -= 1;
+
+        yield return StartCoroutine(DamageEffect());
+        
+        //ステージの移動を元に戻す
+        stage.ScrollSpeed = stage.roadScrollSpeed;
+    }
+
+    [SerializeField]
+    bool _alphaRound = false;
+
+    IEnumerator DamageEffect()
+    {
+        var renderer = GetComponent<Renderer>();
+
+        float interval = 0f;
+        while (interval < 1f)
+        {
+            var angle = interval * Mathf.PI * 4f;
+            var alpha = (Mathf.Sin(angle) * 0.5f) + 0.5f;
+            if (_alphaRound) { alpha = Mathf.RoundToInt(alpha); }
+            
+            interval += Time.deltaTime;
+
+            //renderer.material.color = new Color(1f, 0, 0, sin);
+            var color = Color.red;
+            color.a = alpha;
+            renderer.material.color = color;
+
+            yield return null;
+          
+        }
+
+        renderer.material.color = Color.red;
+    }
+}
