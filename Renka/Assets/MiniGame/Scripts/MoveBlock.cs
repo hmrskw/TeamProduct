93a936353bf1d4ffa2c718570753585c7d5ee612
PS15A05\PS15A kamimura <kamimurayuki0717@gmail.com>
2016-12-12 14:32:50 +0900

    ミニゲームで飛び出す障害物、反復運動する障害物を追加 フリックでジャンプできるように

--- Renka/Assets/MiniGame/Scripts/MoveBlock.cs
+++ Renka/Assets/MiniGame/Scripts/MoveBlock.cs
@@ -3,13 +3,44 @@ using System.Collections;
 
 public class MoveBlock : MonoBehaviour {
     public bool isInRange = false;
-	// Use this for initialization
-	void Start () {
+
+    [Tooltip("障害物が右から左ならTrue、左から右ならfalse")]
+    [SerializeField]
+    private bool isRight = true;
+
+    [SerializeField]
+    private float leftLimitPos;
+
+    [SerializeField]
+    private float rightLimitPos;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+        if(isInRange==true)
+        {
+            if(isRight==true)
+            {
+                if(transform.position.x>=leftLimitPos)
+                {
+                    transform.Translate(-0.1f, 0, 0);
+
+                }
+            }
+
+            else
+            {
+                if (transform.position.x <= rightLimitPos)
+                {
+                    transform.Translate(0.1f, 0, 0);
+                }
+                    
+            }
+        }
 	}
 }
---
+++ Renka/Assets/MiniGame/Scripts/RepeatMoveBlock.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RepeatMoveBlock : MonoBehaviour {
+    [SerializeField]
+    float leftLimitPos;
+
+    [SerializeField]
+    float rightLimitPos;
+    [SerializeField]
+    bool goRight=true;
+    [SerializeField]
+    float speed;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(goRight==false)
+        {
+            if(transform.position.x>=leftLimitPos)
+            {
+                transform.Translate(-speed, 0, 0);
+                
+            }
+            else
+            {
+                goRight = true;
+            }
+        }
+
+    else
+        {
+            if (transform.position.x <= rightLimitPos)
+            {
+                transform.Translate(speed, 0, 0);
+
+            }
+            else
+            {
+                goRight = false;
+            }
+        }
+	}
+}
