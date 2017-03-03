ed136585e5d1fdfdf0b318b31b39ea00e6c618c7
PS15A05\PS15A kamimura <kamimurayuki0717@gmail.com>
2017-03-03 14:26:07 +0900

    競合の解決

---
+++ Renka/Assets/MiniGame/Scripts/ChengeLighting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChengeLighting : MonoBehaviour {
+    [SerializeField]
+    Material skyMorning;
+    [SerializeField]
+    Material skyNight;
+
+    Color nightColor = new Color(0.2f,0.2f,0.17f);
+    Color MorningColor = new Color(0.49f, 0.54f, 0.61f);
+    // Use this for initialization
+    void Start () {
+        if(DataManager.Instance.difficulty == 2)
+        {
+            RenderSettings.skybox = skyNight;
+            RenderSettings.ambientSkyColor = nightColor;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
